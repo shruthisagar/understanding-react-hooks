86a1a1d2d9c0cf96e299819d63040e7758e15037
Shruthi Sagar <sscr27@gmail.com>
2022-01-03 16:36:12 +0400

    #added update property as well in useState hook

--- src/hooksTutorial/UseStateHook.jsx
+++ src/hooksTutorial/UseStateHook.jsx
@@ -3,7 +3,12 @@ import '../index.css'
 
 function UseStateHook (){
     const [count, setCount] = useState(0);
+    const [objectState, setObjectState] = useState({
+        name:'devopedia',
+        age: 4
+    })
     return (
+        <>
         <div className='container'>
             <div className='execution'>
             <h5>
@@ -11,13 +16,17 @@ function UseStateHook (){
             </h5>
             Button Clicked: {count} times
             <div>
-                <button onClick={e=>setCount(count+1)}>Click Button</button>
+                <button onClick={()=>setCount((prevVal)=>{return prevVal+1})}>+</button>
+                <button onClick={()=>setCount((prevVal)=>{return prevVal-1})}>-</button>
             </div>
             <div>
                 <button onClick={e=>setCount(0)}>Reset</button>
             </div>
             </div>
             <div className='codeDisplay'>
+            <h5>
+                The code behind it
+            </h5>
                 <code>
                     {'const [count, setCount] = useState(0);'} 
                     </code>
@@ -26,10 +35,40 @@ function UseStateHook (){
                     <br />
                     <br />
                 <code>
-                    {'<button onClick={e=>setCount(count+1)}>Click Button</button>'}
+                    {'<button onClick={()=>setCount((prevVal)=>{return prevVal+1})}>+</button>'}
                 </code>
             </div>
+            
         </div>
+        <hr />
+        <h5>Updating property</h5>
+        <div className='container'>
+            <div className='execution'>
+            <h5>
+                Understanding the <code>useState</code> Hook
+            </h5>
+            Name: {objectState.name}
+            <br />
+            Age: {objectState.age || 'cleared'}
+            <div>
+                <button onClick={()=>setObjectState((prevVal)=>{return {name:'Shruthi Sagar'}})}>Update Object</button>
+                <button onClick={()=>setObjectState((prevVal)=>{return {...objectState, name:'Shruthi Sagar'}})}>Update Name</button>
+                <button onClick={()=>setObjectState((prevVal)=>{return {name:'devopedia', age: 4}})}>Reset</button>
+            </div>
+            </div>
+            <div className='codeDisplay'>
+            <h5>
+                The code behind it
+            </h5>
+            <code>
+                {"<button onClick={()=>setObjectState((prevVal)=>{return {name:'Shruthi Sagar'}})}>Update Object</button>"} <br/>
+                {"<button onClick={()=>setObjectState((prevVal)=>{return {...objectState, name:'Shruthi Sagar'}})}>Update Name</button>"}<br />
+                {"<button onClick={()=>setObjectState((prevVal)=>{return {name:'devopedia', age: 4}})}>Reset</button>'}"}
+            </code>
+            </div>
+            
+        </div>
+    </>
     )
 }
 export default UseStateHook;
